wooheaven/006759

소스 파일 추가

Unit 59/pointer_add.c
@@ -1,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int numArr[5] = { 11, 22, 33, 44, 55 };
+    int *numPtrA;
+    int *numPtrB;
+    int *numPtrC;
+
+    numPtrA = numArr;    // 배열 첫 번째 요소의 메모리 주소를 포인터에 저장
+
+    numPtrB = numPtrA + 1;    // 포인터 연산
+    numPtrC = numPtrA + 2;    // 포인터 연산
+
+    printf("%p\n", numPtrA);    // 00A3FC00: 메모리 주소. 컴퓨터마다, 실행할 때마다 달라짐
+    printf("%p\n", numPtrB);    // 00A3FC04: sizeof(int) * 1이므로 numPtrA에서 4가 증가함
+    printf("%p\n", numPtrC);    // 00A3FC08: sizeof(int) * 2이므로 numPtrB에서 8이 증가함
+
+    return 0;
+}

Unit 61/return_union_enum.c
@@ -1,0 +1,41 @@
+#include <stdio.h>
+
+union Box {
+    short candy;
+    float snack;
+    char doll[8];
+};
+
+enum BOX_TYPE {
+    BOX_PAPER = 0,
+    BOX_WOOD,
+    BOX_PLASTIC
+};
+
+union Box getBox()    // Box 공용체를 반환하는 getBox 함수 정의
+{
+    union Box b;  // 공용체 변수 선언
+
+    b.candy = 10;
+
+    return b;     // 공용체 변수 반환
+}
+
+enum BOX_TYPE getBoxType()    // BOX_TYPE 열거형을 반환하는 getBoxType 함수 정의
+{
+    return BOX_WOOD;
+}
+
+int main()
+{
+    union Box box;
+    enum BOX_TYPE boxType;
+
+    box = getBox();        // 반환된 공용체 변수의 내용이 b1로 모두 복사됨
+    boxType = getBoxType();    // 반환된 열거형 값이 g1에 저장됨
+
+    printf("%d\n", box.candy);    // 10: getBox에서 저장한 값
+    printf("%d\n", boxType);      //  1: getBoxType에서 반환한 BOX_WOOD
+
+    return 0;
+}

Unit 81/practice_bitmap_asciiart.c
@@ -1,0 +1,142 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+#pragma pack(push, 1)
+
+typedef struct _BITMAPFILEHEADER
+{
+    unsigned short bfType;
+    unsigned int   bfSize;
+    unsigned short bfReserved1;
+    unsigned short bfReserved2;
+    unsigned int   bfOffBits;
+} BITMAPFILEHEADER;
+
+typedef struct _BITMAPINFOHEADER
+{
+    unsigned int   biSize;
+    int            biWidth;
+    int            biHeight;
+    unsigned short biPlanes;
+    unsigned short biBitCount;
+    unsigned int   biCompression;
+    unsigned int   biSizeImage;
+    int            biXPelsPerMeter;
+    int            biYPelsPerMeter;
+    unsigned int   biClrUsed;
+    unsigned int   biClrImportant;
+} BITMAPINFOHEADER;
+
+typedef struct _RGBTRIPLE
+{
+    unsigned char rgbtBlue;
+    unsigned char rgbtGreen;
+    unsigned char rgbtRed;
+} RGBTRIPLE;
+
+#pragma pack(pop)
+
+#define PIXEL_SIZE   3
+#define PIXEL_ALIGN  4
+
+int main()
+{
+    FILE *fpBmp;
+    FILE *fpTxt;
+    BITMAPFILEHEADER fileHeader;
+    BITMAPINFOHEADER infoHeader;
+
+    unsigned char *image;
+    int size;
+    int width, height;
+    int padding;
+
+    char ascii[] = { '#', '#', '@', '%', '=', '+', '*', ':', '-', '.', ' ' };
+
+    fpBmp = fopen("Lenna80x80.bmp", "rb");
+    if (fpBmp == NULL)
+        return 0;
+
+    if (/*(1)_______________________________________________*/ < 1)
+    {
+        fclose(fpBmp);
+        return 0;
+    }
+
+    if (/*(2)__________________*/)
+    {
+        fclose(fpBmp);
+        return 0;
+    }
+
+    if (fread(&infoHeader, sizeof(BITMAPINFOHEADER), 1, fpBmp) < 1)
+    {
+        fclose(fpBmp);
+        return 0;
+    }
+
+    if (infoHeader.biBitCount != 24)
+    {
+        fclose(fpBmp);
+        return 0;
+    }
+
+    size = infoHeader.biSizeImage;
+    width = infoHeader.biWidth;
+    height = infoHeader.biHeight;
+
+    padding = (PIXEL_ALIGN - ((width * PIXEL_SIZE) % PIXEL_ALIGN)) % PIXEL_ALIGN;
+
+    if (size == 0)
+    {
+        size = (width * PIXEL_SIZE + padding) * height;
+    }
+
+    image = malloc(size);
+
+    fseek(fpBmp, fileHeader.bfOffBits, SEEK_SET);
+
+    if (fread(image, size, 1, fpBmp) < 1)
+    {
+        fclose(fpBmp);
+        return 0;
+    }
+
+    fclose(fpBmp);
+
+    fpTxt = fopen("ascii.txt", "w");
+    if (fpTxt == NULL)
+    {
+        free(image);
+        return 0;
+    }
+
+    for (int y = height - 1; y >= 0; y--)
+    {
+        for (int x = 0; x < width; x++)
+        {
+            int index = (x * PIXEL_SIZE) + (y * (width * PIXEL_SIZE)) + (padding * y);
+
+            RGBTRIPLE *pixel = (RGBTRIPLE *)&image[index];
+
+            unsigned char blue = pixel->rgbtBlue;
+            unsigned char green = pixel->rgbtGreen;
+            unsigned char red = pixel->rgbtRed;
+
+            unsigned char gray = (red + green + blue) / PIXEL_SIZE;
+
+            /*(3)____________________________________*/
+
+            fprintf(fpTxt, "%c%c", c, c);
+        }
+
+        fprintf(fpTxt, "\n");
+    }
+
+    fclose(fpTxt);
+
+    free(image);
+
+    return 0;
+}

namjaeyoon/c.dojang

Unit 59/pointer_add.c
@@ -1,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int numArr[5] = { 11, 22, 33, 44, 55 };
+    int *numPtrA;
+    int *numPtrB;
+    int *numPtrC;
+
+    numPtrA = numArr;    // 배열 첫 번째 요소의 메모리 주소를 포인터에 저장
+
+    numPtrB = numPtrA + 1;    // 포인터 연산
+    numPtrC = numPtrA + 2;    // 포인터 연산
+
+    printf("%p\n", numPtrA);    // 00A3FC00: 메모리 주소. 컴퓨터마다, 실행할 때마다 달라짐
+    printf("%p\n", numPtrB);    // 00A3FC04: sizeof(int) * 1이므로 numPtrA에서 4가 증가함
+    printf("%p\n", numPtrC);    // 00A3FC08: sizeof(int) * 2이므로 numPtrA에서 8이 증가함
+
+    return 0;
+}

Unit 61/return_union_enum.c
@@ -1,0 +1,41 @@
+#include <stdio.h>
+
+union Box {
+    short candy;
+    float snack;
+    char doll[8];
+};
+
+enum BOX_TYPE {
+    BOX_PAPER = 0,
+    BOX_WOOD,
+    BOX_PLASTIC
+};
+
+union Box getBox()    // Box 공용체를 반환하는 getBox 함수 정의
+{
+    union Box b;  // 공용체 변수 선언
+
+    b.candy = 10;
+
+    return b;     // 공용체 변수 반환
+}
+
+enum BOX_TYPE getBoxType()    // BOX_TYPE 열거형을 반환하는 getBoxType 함수 정의
+{
+    return BOX_WOOD;
+}
+
+int main()
+{
+    union Box box;
+    enum BOX_TYPE boxType;
+
+    box = getBox();        // 반환된 공용체 변수의 내용이 box로 모두 복사됨
+    boxType = getBoxType();    // 반환된 열거형 값이 boxType에 저장됨
+
+    printf("%d\n", box.candy);    // 10: getBox에서 저장한 값
+    printf("%d\n", boxType);      //  1: getBoxType에서 반환한 BOX_WOOD
+
+    return 0;
+}

Unit 81/practice_bitmap_asciiart.c
@@ -1,0 +1,142 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+#pragma pack(push, 1)
+
+typedef struct _BITMAPFILEHEADER
+{
+    unsigned short bfType;
+    unsigned int   bfSize;
+    unsigned short bfReserved1;
+    unsigned short bfReserved2;
+    unsigned int   bfOffBits;
+} BITMAPFILEHEADER;
+
+typedef struct _BITMAPINFOHEADER
+{
+    unsigned int   biSize;
+    int            biWidth;
+    int            biHeight;
+    unsigned short biPlanes;
+    unsigned short biBitCount;
+    unsigned int   biCompression;
+    unsigned int   biSizeImage;
+    int            biXPelsPerMeter;
+    int            biYPelsPerMeter;
+    unsigned int   biClrUsed;
+    unsigned int   biClrImportant;
+} BITMAPINFOHEADER;
+
+typedef struct _RGBTRIPLE
+{
+    unsigned char rgbtBlue;
+    unsigned char rgbtGreen;
+    unsigned char rgbtRed;
+} RGBTRIPLE;
+
+#pragma pack(pop)
+
+#define PIXEL_SIZE   3
+#define PIXEL_ALIGN  4
+
+int main()
+{
+    FILE *fpBmp;
+    FILE *fpTxt;
+    BITMAPFILEHEADER fileHeader;
+    BITMAPINFOHEADER infoHeader;
+
+    unsigned char *image;
+    int size;
+    int width, height;
+    int padding;
+
+    char ascii[] = { '#', '#', '@', '%', '=', '+', '*', ':', '-', '.', ' ' };
+
+    fpBmp = fopen("Peppers80x80.bmp", "rb");
+    if (fpBmp == NULL)
+        return 0;
+
+    if (/*(1)_______________________________________________*/ < 1)
+    {
+        fclose(fpBmp);
+        return 0;
+    }
+
+    if (/*(2)__________________*/)
+    {
+        fclose(fpBmp);
+        return 0;
+    }
+
+    if (fread(&infoHeader, sizeof(BITMAPINFOHEADER), 1, fpBmp) < 1)
+    {
+        fclose(fpBmp);
+        return 0;
+    }
+
+    if (infoHeader.biBitCount != 24)
+    {
+        fclose(fpBmp);
+        return 0;
+    }
+
+    size = infoHeader.biSizeImage;
+    width = infoHeader.biWidth;
+    height = infoHeader.biHeight;
+
+    padding = (PIXEL_ALIGN - ((width * PIXEL_SIZE) % PIXEL_ALIGN)) % PIXEL_ALIGN;
+
+    if (size == 0)
+    {
+        size = (width * PIXEL_SIZE + padding) * height;
+    }
+
+    image = malloc(size);
+
+    fseek(fpBmp, fileHeader.bfOffBits, SEEK_SET);
+
+    if (fread(image, size, 1, fpBmp) < 1)
+    {
+        fclose(fpBmp);
+        return 0;
+    }
+
+    fclose(fpBmp);
+
+    fpTxt = fopen("ascii.txt", "w");
+    if (fpTxt == NULL)
+    {
+        free(image);
+        return 0;
+    }
+
+    for (int y = height - 1; y >= 0; y--)
+    {
+        for (int x = 0; x < width; x++)
+        {
+            int index = (x * PIXEL_SIZE) + (y * (width * PIXEL_SIZE)) + (padding * y);
+
+            RGBTRIPLE *pixel = (RGBTRIPLE *)&image[index];
+
+            unsigned char blue = pixel->rgbtBlue;
+            unsigned char green = pixel->rgbtGreen;
+            unsigned char red = pixel->rgbtRed;
+
+            unsigned char gray = (red + green + blue) / PIXEL_SIZE;
+
+            /*(3)____________________________________*/
+
+            fprintf(fpTxt, "%c%c", c, c);
+        }
+
+        fprintf(fpTxt, "\n");
+    }
+
+    fclose(fpTxt);
+
+    free(image);
+
+    return 0;
+}