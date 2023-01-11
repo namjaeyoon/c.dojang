5ccea13b0c65e3ecb760b666cad8ed2a87a89a7e
Jae-Yoon Nam <user@localhost>
2023-01-11 15:30:18 +0900

    Visual Studio 2022로 주석 수정

--- Unit 38/variable_length_array.c
+++ Unit 38/variable_length_array.c
@@ -7,7 +7,7 @@ int main()
 
     scanf("%d", &size);  // 배열의 크기를 입력받음
 
-    int numArr[size];    // GCC에서는 사용 가능, Visual Studio 2015에서는 컴파일 에러
+    int numArr[size];    // GCC에서는 사용 가능, Visual Studio 2022에서는 컴파일 에러
 
     return 0;
 }
--- Unit 85/gets_s_puts.c
+++ Unit 85/gets_s_puts.c
@@ -5,7 +5,7 @@ int main()
     char buffer[100];
 
     gets_s(buffer, sizeof(buffer));    // 표준 입력에서 문자열을 입력받음
-                                       // Visual Studio 2015
+                                       // Visual Studio 2022
 
     puts(buffer);    // 문자열을 화면(표준 출력)에 출력
 
